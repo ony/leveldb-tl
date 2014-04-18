238810953e1da6a4c37cb20e8dfdfcf5bfe61f67
Nikolay Orlyuk <virkony@gmail.com>
2014-04-18 20:35:47 +0300

    FindLevelDB work in ubuntu and util.hpp improve
    - FindLevelDB now links with snappy if finds
    - util.hpp now have ASSERT/EXPECT for specific status

--- test/util.hpp
+++ test/util.hpp
@@ -9,17 +9,23 @@ namespace leveldb {
     { *os << ::testing::PrintToString(s.ToString()); }
 }
 
-inline ::testing::AssertionResult ok(leveldb::Status s)
+inline ::testing::AssertionResult check(
+    const leveldb::Status &s,
+    std::function<bool(const leveldb::Status &)> pred = [](const leveldb::Status &s) { return s.ok(); }
+)
 {
-    if (s.ok()) return ::testing::AssertionSuccess() << s.ToString();
+    if (pred(s)) return ::testing::AssertionSuccess() << s.ToString();
     else return ::testing::AssertionFailure() << s.ToString();
 }
 
 constexpr bool hostIsNetwork()
 { return *reinterpret_cast<const short*>("\x01\x02") == 0x0102; }
 
-#define ASSERT_OK(E) ASSERT_TRUE(ok(E))
-#define EXPECT_OK(E) EXPECT_TRUE(ok(E))
+#define ASSERT_OK(E) ASSERT_TRUE(check(E))
+#define EXPECT_OK(E) EXPECT_TRUE(check(E))
 
-#define ASSERT_FAIL(E) ASSERT_FALSE(ok(E))
-#define EXPECT_FAIL(E) EXPECT_FALSE(ok(E))
+#define ASSERT_FAIL(E) ASSERT_FALSE(check(E))
+#define EXPECT_FAIL(E) EXPECT_FALSE(check(E))
+
+#define ASSERT_STATUS(M, E) ASSERT_TRUE(check(E, [](const leveldb::Status &s) { return s.Is ## M(); }))
+#define EXPECT_STATUS(M, E) EXPECT_TRUE(check(E, [](const leveldb::Status &s) { return s.Is ## M(); }))
