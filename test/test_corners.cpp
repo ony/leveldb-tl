2304e16f5700c691dd725f677d9627c8867e5bb7
Nikolay Orlyuk <virkony@gmail.com>
2014-04-22 09:54:34 +0300

    expose issues with walking while modifying
    - insert into transaction is hid from walker if it happens before
      position where cover currently points to (even ifcurrent key of txn
      walker is lower)
    - delete of element that txn walker currently points to doesn't work if
      that's element from base collection

--- test/test_corners.cpp
+++ test/test_corners.cpp
@@ -8,6 +8,7 @@
 #include "util.hpp"
 
 using namespace std;
+using ::testing::PrintToString;
 
 TEST(TestTxnIterator, memory_walk_deleted)
 {
@@ -52,7 +53,7 @@ TEST(TestTxnIterator, whiteout_walk_deleted)
     EXPECT_EQ( "b", w.key() );
 }
 
-TEST(TestTxnIterator, DISABLED_txn_walk_deleted_delete)
+TEST(TestTxnIterator, txn_walk_deleted_delete)
 {
     leveldb::MemoryDB mem {
         { "b", "1" },
@@ -84,4 +85,74 @@ TEST(TestTxnIterator, DISABLED_txn_walk_deleted_delete)
     ASSERT_TRUE( w.Valid() );
     EXPECT_EQ( "b", w.key() );
     EXPECT_EQ( "1", w.value() );
+
+    w.Next();
+    ASSERT_TRUE( w.Valid() );
+    EXPECT_EQ( "c", w.key() );
+    EXPECT_EQ( "3", w.value() );
+
+    w.Next();
+    EXPECT_FALSE( w.Valid() ) << "Still have key " << PrintToString(w.key());
+}
+
+TEST(TestTxnIterator, DISABLED_txn_walk_deleted)
+{
+    leveldb::MemoryDB mem {
+        { "b", "1" },
+        { "a", "2" },
+        { "c", "3" },
+    };
+
+    auto txn = leveldb::transaction(mem);
+
+    std::string v;
+
+    auto w = leveldb::walker(txn);
+    w.SeekToFirst();
+
+    ASSERT_TRUE( w.Valid() );
+    EXPECT_EQ( "a", w.key() );
+
+    EXPECT_OK( txn.Delete("a") );
+    EXPECT_STATUS( NotFound, txn.Get("a", v) );
+    EXPECT_OK( mem.Get("a", v) );
+    ASSERT_TRUE( w.Valid() );
+    EXPECT_EQ( "b", w.key() );
+}
+
+// insert right between walkers over database and transaction
+TEST(TestTxnIterator, DISABLED_txn_insert_next)
+{
+    leveldb::MemoryDB mem {
+        { "a", "2" },
+        { "d", "4" },
+    };
+
+    auto txn = leveldb::transaction(mem);
+
+    std::string v;
+
+    auto w = leveldb::walker(txn);
+    EXPECT_OK( txn.Put("c", "3") );
+    w.SeekToFirst();
+
+    ASSERT_TRUE( w.Valid() );
+    EXPECT_EQ( "a", w.key() );
+
+    EXPECT_OK( txn.Put("b", "1") );
+
+    ASSERT_TRUE( w.Valid() );
+    EXPECT_EQ( "a", w.key() );
+
+    w.Next();
+    ASSERT_TRUE( w.Valid() );
+    EXPECT_EQ( "b", w.key() );
+
+    w.Next();
+    ASSERT_TRUE( w.Valid() );
+    EXPECT_EQ( "c", w.key() );
+
+    w.Next();
+    ASSERT_TRUE( w.Valid() );
+    EXPECT_EQ( "d", w.key() );
 }
